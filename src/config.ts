04b486a3b2df4a53cd87bc46e8b289f31b5c6a06
Leandro Acquati <leandroacquati@gmail.com>
2021-11-05 16:47:37 -0300

    update config

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const GameConfig: Phaser.Types.Core.GameConfig = {
   version: '1.0',
   width: 800,
   height: 600,
-  backgroundColor: 0x3a404d,
+  backgroundColor: '#0x3a404d',
   type: Phaser.AUTO,
   parent: 'game',
   physics: {
